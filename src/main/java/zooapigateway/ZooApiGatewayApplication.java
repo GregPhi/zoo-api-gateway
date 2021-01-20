20ef56e05a278be7d05241ec8d4ad617bd5a1ba1
GregPhi <gregoire.philippot@laoste.net>
2021-01-20 17:50:18 +0100

    initialize : project

---
+++ src/main/java/zooapigateway/ZooApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package zooapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooApiGatewayApplication.class, args);
+	}
+
+}
